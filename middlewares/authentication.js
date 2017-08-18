a201a081e3c2fef3a594b09036e3029edec2fe44
Don Byington <don@dbyington.com>
2017-08-18 15:14:04 +0200

    fix: authentication

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,8 +1,9 @@
 'use strict';
+
 require('dotenv').config();
-// const fetch = require('node-fetch');
 const request = require('request-promise');
 
+const UserSerializer = require('../utils/user-serializer');
 const User = require('../models/user.model');
 const db = require('../db');
 
@@ -21,24 +22,20 @@ async function _getAccessToken (ctx) {
     form: form,
     json: true
   };
-  console.log('post options:',postOptions);
   let responseBody = await request.post(postOptions)
     .then(async data => {
-      console.log('access token reply', data);
       if (!data.user.id) ctx.throw(401, 'unauthorized');
       let user = await User.findOne({id: data.user.id});
-      console.log('user:',user);
       if (user) {
-        console.log('update access token:', user['id'], data['access_token']);
-        user = await User.update(
+        await User.update(
           {id: user['id']},
           {$set: {access_token: data['access_token']}},
           {new: true},
           function (err, d) {
-            if (err) ctx.throw(500, JSON.stringify({error: {status: 500, error_message: err}}));
-            return d;
+            if (err) ctx.throw(501, JSON.stringify({error: {status: 501, error_message: err}}));
           }
         );
+        user = await User.findOne({id: data.user.id});
       } else {
         const newUser = new User({
           access_token: data.access_token,
@@ -48,36 +45,28 @@ async function _getAccessToken (ctx) {
           profile_picture: data.user.profile_picture
         });
         try {
-          console.log('save new user:', newUser);
           user = await newUser.save();
         } catch (e) {
           ctx.throw(500, JSON.stringify({error: {status: 500, error_message: e}}));
         }
       }
       ctx.state.accessToken = user.access_token;
-      console.log('token:',ctx.state.accessToken);
       return user;
     })
     .catch( err => {
       ctx.throw(401, 'unauthorized sucka', err);
     });
   ctx.status = 200;
-  ctx.body = responseBody;
+  ctx.body = UserSerializer.serializeWithToken(responseBody);
 }
 
 async function checkAuth (ctx, next) {
-  console.log('check for params code and header.authorization');
   if (ctx.query && ctx.query.code) {
-    console.log(ctx.query.code);
     await _getAccessToken(ctx);
   } else if (!ctx.header.authorization) {
     ctx.throw(401, 'unauthorized');
   }
-  // made it through the gaunlet, check the access_token
-
-  // const accessToken = ctx.header.authorization.split(' ')[1];
   if (!ctx.state.accessToken) {
-    console.log('accessToken not defined');
     const user = User.find({access_token: accessToken});
     if (!user['access_token']) {
       // not a valid access_token
