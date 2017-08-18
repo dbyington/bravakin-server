c051a24425e870fb5b55984299c0645f029c898c
Don Byington <don@dbyington.com>
2017-08-18 10:36:25 +0200

    wip: working on authentication

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,82 +1,91 @@
 'use strict';
 require('dotenv').config();
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 const request = require('request-promise');
 
 const User = require('../models/user.model');
 const db = require('../db');
 
-const _getAccessToken = (ctx) => {
+async function _getAccessToken (ctx) {
   const form = {
-    client_id = process.env.CLIENT_ID,
-    client_secret = process.env.CLIENT_SECRET,
-    grant_type = 'authorization_code',
-    redirect_uri = 'https://me/authorize',
-    code = ctx.params.code
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    grant_type: 'authorization_code',
+    redirect_uri: 'http://localhost:3000/authorize',
+    code: ctx.query.code
   };
 
   const postOptions = {
     url: process.env.URL,
     method: 'POST',
-    form: form
-  }
+    form: form,
+    json: true
+  };
+  console.log('post options:',postOptions);
   let responseBody = await request.post(postOptions)
-    .then(data => {
-      data = JSON.parse(data);
+    .then(async data => {
+      console.log('access token reply', data);
       if (!data.user.id) ctx.throw(401, 'unauthorized');
-
-      // insert or update user with access_token
-      // create complete user object to return
       let user = await User.findOne({id: data.user.id});
-      if (user.id) { // user found, update the access_token
-        user = await User.findByIdAndUpdate(user._id,
-          { $set: {access_token: data.access_token}},
+      console.log('user:',user);
+      if (user) {
+        console.log('update access token:', user['id'], data['access_token']);
+        user = await User.update(
+          {id: user['id']},
+          {$set: {access_token: data['access_token']}},
           {new: true},
-          function(err, d) {
+          function (err, d) {
             if (err) ctx.throw(500, JSON.stringify({error: {status: 500, error_message: err}}));
             return d;
           }
         );
       } else {
-        const newUser = new User(
+        const newUser = new User({
           access_token: data.access_token,
           id: data.user.id,
           username: data.user.username,
           full_name: data.user.full_name,
           profile_picture: data.user.profile_picture
-        );
+        });
         try {
+          console.log('save new user:', newUser);
           user = await newUser.save();
         } catch (e) {
           ctx.throw(500, JSON.stringify({error: {status: 500, error_message: e}}));
         }
       }
-
       ctx.state.accessToken = user.access_token;
+      console.log('token:',ctx.state.accessToken);
       return user;
     })
     .catch( err => {
-      ctx.throw(401, 'unauthorized');
+      ctx.throw(401, 'unauthorized sucka', err);
     });
-
   ctx.status = 200;
   ctx.body = responseBody;
 }
 
-modules.exports.checkAuth = async (ctx, next) => {
+async function checkAuth (ctx, next) {
   console.log('check for params code and header.authorization');
-  if (ctx.params.code) {
-    _getAccessToken(ctx);
+  if (ctx.query && ctx.query.code) {
+    console.log(ctx.query.code);
+    await _getAccessToken(ctx);
   } else if (!ctx.header.authorization) {
     ctx.throw(401, 'unauthorized');
   }
   // made it through the gaunlet, check the access_token
-  const accessToken = ctx.header.authorization.split(' ')[1];
-  const user = User.find({access_token: accessToken});
-  if (!user['access_token']) {
-    // not a valid access_token
-    ctx.throw(401, 'unauthorized');
+
+  // const accessToken = ctx.header.authorization.split(' ')[1];
+  if (!ctx.state.accessToken) {
+    console.log('accessToken not defined');
+    const user = User.find({access_token: accessToken});
+    if (!user['access_token']) {
+      // not a valid access_token
+      ctx.throw(401, 'unauthorized');
+    }
+    ctx.state.accessToken = accessToken;
   }
-  ctx.state.accessToken = accessToken;
   next();
 }
+
+module.exports = checkAuth;
