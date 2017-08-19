e0a6e25c4fee643e3dcca7845227b8ba14df9529
Angelica Valenta <angelicavalenta@gmail.com>
2017-08-19 10:49:03 +0200

    feat: wip encyption

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const crypto = require('crypto');
+const crypt = require('../utils/crypto');
 
 const userSchema = new Schema({
   id: Number,
@@ -8,8 +10,12 @@ const userSchema = new Schema({
   profile_picture: String,
   be_like: Array,
   like_tags: Array,
-  access_token: String
+  access_token: String,
+  favorite: String
 });
+
+userSchema.pre('save', crypt.updateFavorite);
+userSchema.pre('update', crypt.updateFavorite);
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
